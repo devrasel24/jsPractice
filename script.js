c75820b5f5f725f70d87835d052b2ddf07a0aa95
MD: RASEL SARKER <developerrasel24@gmail.com>
2021-10-04 21:50:47 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+
+// let pragraph = 'this is Pragraph TAG'
+// console.log(pragraph.toLocaleUpperCase())
+
+
+// let pragraph = 'this is Pragraph TAG'
+// console.log(pragraph.indexOf('TAG'))
+
+// let pragraph = 'this is Pragraph TAG'
+// console.log(pragraph.split(' '))
+
+// let number1 = 20;
+// let number2 = 25.5;
+// number2 = ''+ number2;
+
+// console.log( typeof number2)
+
+
+// let number1 = .1;
+// let number2 = .2;
+ 
+// let total = (number1 + number2)
+// total = total.toFixed(35);
+// console.log(total)
+
+let number1 = 14;
+let number2 = 6;
+number2++;
+let total = (number2 / number1);
+
+console.log(total);
+
